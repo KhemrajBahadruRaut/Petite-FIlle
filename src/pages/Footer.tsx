088a8e7887dc1a05b92974222f14d69bc9c7fc50
AmanRaut <amanraut76@gmail.com>
2025-09-04 11:29:25 +0545

    started Pettie fille project

---
+++ src/pages/Footer.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+
+
+
+
+
+const Footer = () => {
+  return (
+    <div>
+      this is footer content
+    </div>
+  )
+}
+
+export default Footer
---
+++ src/pages/Header.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+
+
+
+const Header = () => {
+  return (
+    <div className='border border-black p-2 mb-2'>
+      this is Header Part
+    </div>
+  )
+}
+
+export default Header
